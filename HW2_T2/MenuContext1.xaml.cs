0b45cd692e04587eb19b944d60c068fd245ca356
omerov islam <islamomerov@outlook.com>
2019-04-25 20:38:24 +0400

    123

---
+++ HW2_T2/MenuContext1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HW2_T2
+{
+    /// <summary>
+    /// Interaction logic for MenuContext1.xaml
+    /// </summary>
+    public partial class MenuContext1 : Window
+    {
+        public MenuContext1()
+        {
+            InitializeComponent();
+        }
+    }
+}
